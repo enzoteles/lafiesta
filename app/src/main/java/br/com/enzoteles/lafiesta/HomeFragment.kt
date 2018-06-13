1a024ae12dd33390b56568bf3da09ce040f928f3
Enzo de Carvalho Teles <enzoteles@localhost.localdomain>
2018-06-13 09:19:21 -0300

    ajuste na tela de splash e home

--- app/src/main/java/br/com/enzoteles/lafiesta/HomeFragment.kt
+++ app/src/main/java/br/com/enzoteles/lafiesta/HomeFragment.kt
@@ -1,39 +1,90 @@
 package br.com.enzoteles.lafiesta
 
 import android.arch.lifecycle.MutableLiveData
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.support.annotation.RequiresApi
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import br.com.enzoteles.quickhelp.fragment.HelpFragment
+import br.com.enzoteles.quickhelp.log.HelpLog
+import br.com.enzoteles.quickhelp.security.HelpSecurity
+import br.com.lafiestasdk.gps.GPSTracker
 import br.com.lafiestasdk.webservice.api.FourSquareApi
 import br.com.lafiestasdk.webservice.domain.FourSquareJson
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
 class HomeFragment: HelpFragment(){
+
+    lateinit var mGPS: GPSTracker
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         var view = inflater!!.inflate(R.layout.fragment_home, container, false)
         return view
     }
 
+    @RequiresApi(Build.VERSION_CODES.M)
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        initSplash()
+        initGPS()
+        initUI()
+    }
+
+    /**
+     * method to start splash and set visibility in home layout
+     * */
+    private fun initSplash() {
+
+        Handler().postDelayed({
+            ll_splash.visibility = View.GONE
+            ll_home.visibility = View.VISIBLE
+        }, 3000)
+    }
+
+    /**
+     * method to start GSP
+     */
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun initGPS() {
+
+        mGPS = GPSTracker(activity.baseContext)
+        if(mGPS.canGetLocation()){
+            mGPS.getLocation()
+        }else{
+            HelpLog.error("Unabletofind")
+        }
+    }
+
+    /**
+     * method to start UI
+     * */
+    private fun initUI() {
+
+
         var data: MutableLiveData<FourSquareJson>?=  MutableLiveData<FourSquareJson>()
-        val api = FourSquareApi("-23.5428633,-46.8824385")
+        var latlng = "${mGPS.getLatitude()},${mGPS.getLongitude()}"
+
+        HelpLog.info("${mGPS.getLatitude()} -- ${mGPS.getLongitude()}")
+        val api = FourSquareApi(latlng)
         api.consultarNight()
                 ?.subscribeOn(Schedulers.io())
                 ?.observeOn(AndroidSchedulers.mainThread())
                 ?.subscribe ({ response ->
                     data?.value = response as FourSquareJson?
-                    Log.i("data>>> ", "${data?.value?.response?.venues!![0].name}")
+                    Log.i("data teste>>> ", "${data?.value?.response?.venues!![0].name}")
                 }, { e ->
                     e.printStackTrace()
                 })
     }
+
+
 
 }
--- app/src/main/java/br/com/enzoteles/lafiesta/MainActivity.kt
+++ app/src/main/java/br/com/enzoteles/lafiesta/MainActivity.kt
@@ -4,20 +4,28 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import br.com.enzoteles.quickhelp.fragment.HelpManagerFragment
 import br.com.enzoteles.quickhelp.security.HelpSecurity
+import br.com.lafiestasdk.gps.GPSTracker
 
 class MainActivity : AppCompatActivity() {
 
 
     //variable
-    lateinit var splash:SplashFragment
     lateinit var manager: HelpManagerFragment
+    lateinit var home: HomeFragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initUI()
+    }
+
+    private fun initUI() {
+
         manager = HelpManagerFragment(this)
         HelpSecurity.manager = manager
-        splash = SplashFragment()
-        manager!!.addFragment(R.id.content, splash, "splash", false)
+        home = HomeFragment()
+        manager!!.addFragment(R.id.content, home, "home", false)
     }
+
 }
